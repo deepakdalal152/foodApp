300e12da67a88d24025ac8710c056a51b4a2f297
deepakdalal152 <40324333+deepakdalal152@users.noreply.github.com>
2022-06-08 08:25:42 +0530

    Add files via upload

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+
+function navbar()
+{
+    return `    <div id="navbar">
+                    <div id="recipeOfTheDay">Recipe of the Day</div>
+                    <div id="randomeRecipe"> Random Recipe</div>
+                    <div id="searchRecipeBox">
+                        <input type="text" name="Search Recipe" id="searchRecipeInput" placeholder="Enter Recipe Name....">
+                        <button id="searchRecipe">Search</button>
+                    </div>
+                </div>`
+}
+
+
+
+export {navbar}
